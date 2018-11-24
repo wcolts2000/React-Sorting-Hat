d870df9291422b77f71fc9beacb2cd61aaef7611
wcolts2000 <wcolts2000@gmail.com>
2018-11-24 09:27:26 -0800

    added house history to house data

--- react-sorting-hat/src/components/HouseData.js
+++ react-sorting-hat/src/components/HouseData.js
@@ -2,22 +2,26 @@ const houseData = {
   hufflepuff: {
     colors: ['Yellow', 'Black'],
     wizards: ['Helga Hufflepuff', 'Hengist of Woodcroft (founder of Hogsmeade)', 'Newt Scamander', 'Artemisia Lufkin (first female minister for magic)'],
-    traits: ['hard work', 'dedication', 'patience', 'loyalty', 'fair play']
+    traits: ['hard work', 'dedication', 'patience', 'loyalty', 'fair play'],
+    history: 'Your emblematic animal is the badger. Pomona Sprout was the Head of Hufflepuff during 1991-1998, Sprout left the post of Head of Hufflepuff and Herbology Professor sometime before 2017 and her successor for the position of Head of Hufflepuff is currently unknown. The Fat Friar is its ghost. The founder of the house was Helga Hufflepuff. Hufflepuff corresponds to the element of earth. Your dormitories and common room are located somewhere in the basement, near the castle\'s kitchens. It can be accessed by tapping the barrel two from the bottom, middle of the second row in the rhythm of "Helga Hufflepuff" and is described as being a cosy and welcoming place with yellow hangings, fat armchairs, and underground tunnels that lead to the dormitories, which have perfectly round doors, similar to barrel tops.'
   },
   ravenclaw: {
     colors: ['blue', 'bronze'],
     wizards: ['Rowena Ravenclaw', ' Gilderoy Lockheart', 'Ignatia Wildsmith (inventor of Floo powder)', 'Garrick Ollivander'],
-    traits: ['intelligence', 'knowledge', 'wit']
+    traits: ['intelligence', 'knowledge', 'wit'],
+    history: ' Your emblematic animal is the eagle. The Ravenclaw Head of House in the 1990s was Filius Flitwick. The ghost of Ravenclaw is the Grey Lady, who was the daughter of Rowena Ravenclaw, the house\'s founder. Ravenclaw corresponds to the element of air. The Ravenclaw common room and dormitories are located in a tower on the west side of the castle. Ravenclaw students must answer a riddle as opposed to giving a password to enter their dormitories. This riddle, however, can be answered by non-Ravenclaws.'
   },
   gryffindor: {
     colors: ['scarlet', 'gold'],
     wizards: ['Godric Gryffindor', 'Minerva McGonagall', 'Albus Dumbledore', 'Harry Potter', 'Celestina Warbeck'],
-    traits: ['bravery', 'daring', 'nerve', 'chivalry']
+    traits: ['bravery', 'daring', 'nerve', 'chivalry'],
+    history: ' Your emblematic animal is the lion. Minerva McGonagall was the most recent Head of Gryffindor. Sir Nicholas de Mimsy-Porpington, also known as "Nearly Headless Nick", is the house ghost. The founder of the house was Godric Gryffindor. Gryffindor corresponds to the element of Fire. The common room is located in one of the highest towers at Hogwarts, the entrance is situated on the seventh floor in the east wing of the castle and is guarded by a portrait of The Fat Lady. She permits entrance if given the correct password, which is changed numerous times throughout the school year.'
   },
   slytherin: {
     colors: ['emerald green', 'silver'],
     wizards: ['Salazar Slytherin', 'Merlin', 'Tom Riddle', 'Dolores Umbridge'],
-    traits: ['ambition', 'cunning',  'resourcefulness']
+    traits: ['ambition', 'cunning',  'resourcefulness'],
+    history: ' Your emblematic animal is the serpent. Professor Horace Slughorn was the Head of Slytherin during the 1997–1998 school year, replacing Severus Snape, who as well, replaced Slughorn as Potions Professor when he retired for the first time several years prior. The Bloody Baron is the house ghost. The founder of the house was Salazar Slytherin. Slytherin corresponds roughly to the element of water. The Slytherin dormitories and common room are reached through a bare stone wall in the Dungeons. The Slytherin common room lies beneath the Black Lake. It is a long, low underground room with rough stone walls and silver lamps hanging from the ceiling.'
   }
 }
 
