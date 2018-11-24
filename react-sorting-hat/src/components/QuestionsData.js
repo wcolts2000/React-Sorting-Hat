645d2251b97271e52020fde887065801fd0a3c6d
wcolts2000 <wcolts2000@gmail.com>
2018-11-24 08:59:14 -0800

    finished questions data

---
+++ react-sorting-hat/src/components/QuestionsData.js
@@ -0,0 +1,44 @@
+const sortingQuestions = {
+  question1: {
+    question: "You stumble upon a long forgotten history book. While thumbing through the content you find yourself drawn to which chapter first?",
+    r: "The Origin of Sprites and Gnomes",
+    s: "The Seige of Finrynne's Isle",
+    g: "The Adventures of Gaulphyneous, Dragon Trainer",
+    h: "Anntymnia, The Building of an Empires Capital",
+  },
+  question2: {
+    question: "When you graduate, you most want to be which of the following?",
+    r: "A Professor at Hogwarts",
+    s: "Head of the Wizarding Ministry",
+    h: "Herbology Tonics and Tinctures Specialist",
+    g: "Traveling Wizard",
+  },
+  question3: {
+    question: "A troll has broken into the hall and everyone starts to run. You ...",
+    g: "grab your wand and taunt the troll",
+    s: "incinerate the rope holding the chandelier over the troll with your wand",
+    r: "conjure some sheep, knowing you can lure the troll away by easily distracting it",
+    h: "get everyone to work together to fight off the troll",
+  },
+  question4: {
+    question: "You are presented with 4 scrolls. Which do you choose?",
+    r: "This one has the seal of the ancients",
+    s: "This one is inscribed with the message: Only the worthy may read",
+    g: "This one looks like it may bite you",
+    h: "This one looks exceptionally long",
+  },
+  question5: {
+    question: "Which extracurricular class do you most look forward to?",
+    g: "Wand Combat Training",
+    h: "Mandrake Gardening",
+    r: "Wizarding Trivia",
+    s: "Ministry Debate",
+  },
+  question6: {
+    question: "What nickname will you most like to receive from your peers",
+    s: "The Great",
+    r: "The Sage",
+    g: "The Lionheart",
+    h: "The Just",
+  },
+}
