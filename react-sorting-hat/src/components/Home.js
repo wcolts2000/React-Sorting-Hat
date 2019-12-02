ca1fb75cef4e33a32f72d3dcf395378023a6b041
wcolts2000 <wcolts2000@gmail.com>
2019-12-02 10:32:59 -0800

    removed extra button around link in home page and shrunk train image again

--- react-sorting-hat/src/components/Home.js
+++ react-sorting-hat/src/components/Home.js
@@ -4,6 +4,21 @@ import img from "../images/train.jpg";
 import hat from "../images/sortingHat.png";
 import { Link } from "react-router-dom";
 
+export default function Home() {
+  return (
+    <StyledApp>
+      <StyledH1>Welcome</StyledH1>
+      <Span>to</Span>
+      <StyledH1 type="bottom">Hogwarts</StyledH1>
+      <StyledH2>Let the Sorting Begin</StyledH2>
+      <StyledImg src={hat} />
+      <StyledButton to="/sorting">Come... Let's Find Your House
+      </StyledButton>
+    </StyledApp>
+  );
+}
+
+
 const StyledApp = styled.div`
   text-align: center;
   font-family: "Cinzel", serif;
@@ -61,15 +76,17 @@ const StyledImg = styled.img`
   pointer-events: none;
 `;
 
-const StyledButton = styled.button`
+const StyledButton = styled(Link)`
   padding: 1rem 2rem;
   background: rgba(0,0,0,.7);
   font-size: 1.6rem;
   border: 2px solid yellow;
   color: yellow;
   transition: all 0.3s ease-in;
+  color: yellow;
   font-family: "Cinzel", serif;
   font-weight: 700;
+  text-decoration: none;
   letter-spacing: 3px;
   overflow: hidden;
 
@@ -86,26 +103,9 @@ const StyledButton = styled.button`
     box-shadow: -1px 2px 5px rgba(0, 0, 0, 0.5);
   }
 
-  & > a {
-    text-decoration: none;
-    color: yellow;
+  /* & > a {
   }
   &:hover > a {
     color: black;
-  }
+  } */
 `;
-
-export default function Home() {
-  return (
-    <StyledApp>
-      <StyledH1>Welcome</StyledH1>
-      <Span>to</Span>
-      <StyledH1 type="bottom">Hogwarts</StyledH1>
-      <StyledH2>Let the Sorting Begin</StyledH2>
-      <StyledImg src={hat} />
-      <StyledButton>
-        <Link to="/sorting">Come... Let's Find Your House</Link>
-      </StyledButton>
-    </StyledApp>
-  );
-}
